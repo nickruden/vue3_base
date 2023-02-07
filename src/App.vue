cf95bd4670ce0265d88aec8c0279e22953020305
nickruden <1nikitos.ru2018@gmail.com>
2023-02-07 22:20:02 +0300

    Работа с атрибутами в фреймворке Vue(Task_5)

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,36 @@
  export default{
   name: "App",
   data() {
-	  return {
-		  name: 'Nikita',
-		  surn: 'Rudenko',
-	}
+	return {
+		text: 'page',
+		href: 'page.html',
+	  }
+  }
 }
- }
 </script>
 
 <template>
     <div id="main">
-      <div class="div1">
-      {{ name }}
-      </div>
-      <div class="div2">
-      {{ surn }}
-      </div>
+      <a :href="href">{{ text }}</a>
     </div>
 </template>
 
 <style>
   #main{
     background-color: rgb(43, 221, 117);
-    color: rgb(32, 80, 27);
+    color: rgb(172, 255, 159);
     width: 1000px;
     height: 500px;
     text-align: center;
     border: 10px solid rgb(43, 221, 117);
     border-radius:50px;
-    text-shadow: 0px 1px 20px rgb(46, 116, 39);
+    text-shadow: 0px 1px 21px rgb(140, 247, 197);
   }
-  #main .div1, #main .div2{
-    font-size: 40px;
-    margin-top: 20px;
+  #main a{
+    font-size: 100px;
   }
-  #main .div1{
-    margin-top: 140px;
-  }
-  #main .div2{
-    margin-top: -20px;
+  #main a:hover{
+    color: rgb(126, 186, 255);
+    text-shadow: 0px 1px 21px rgb(37, 56, 85);
   }
 </style>
