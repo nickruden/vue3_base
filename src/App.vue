b43f960204a3c49268c41005ecceacc86f9654db
nickruden <1nikitos.ru2018@gmail.com>
2023-02-07 22:35:53 +0300

    Выполнения кода в представлениях Vue(Task_6)

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
   name: "App",
   data() {
 	return {
-		text: 'page',
-		href: 'page.html',
+    //Ex. 1
+		num: 5,
+
+    //Ex. 2
+    num1: 1,
+		num2: 2,
+		num3: 3,
 	  }
   }
 }
@@ -12,26 +17,35 @@
 
 <template>
     <div id="main">
-      <a :href="href">{{ text }}</a>
+      <!-- Ex. 1  -->
+      <h2>1) Квадрат из числа 5 = {{ num ** 2 }}</h2>
+
+      <!-- Ex. 2 -->
+      <h2>2) Сумма чисел {{ num1}}, {{num2 }}, {{ num3 }} равна {{ num1 + num2 + num3 }}</h2>
+
+      <p>by NikitaRudenko</p>
     </div>
 </template>
 
 <style>
   #main{
     background-color: rgb(43, 221, 117);
-    color: rgb(172, 255, 159);
+    color: rgb(189, 255, 177);
     width: 1000px;
     height: 500px;
     text-align: center;
     border: 10px solid rgb(43, 221, 117);
     border-radius:50px;
-    text-shadow: 0px 1px 21px rgb(140, 247, 197);
+    text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
-  #main a{
-    font-size: 100px;
+  #main h2{
+    font-size: 40px;
+    margin-top: 20px;
   }
-  #main a:hover{
-    color: rgb(126, 186, 255);
-    text-shadow: 0px 1px 21px rgb(37, 56, 85);
+  #main p{
+    font-size: 30px;
+    margin-top: 240px;
+    color: rgb(83, 100, 80);
+    text-shadow: 0px 1px 10px rgb(128, 151, 141);
   }
 </style>
