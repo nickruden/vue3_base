7c815dd0be532d10760175a7cf1a5b2ba58203ab
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 17:08:52 +0300

    Работа с data внутри методов Vue(Task_11)

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,9 @@
   name: "App",
   data() {
 	return {
-	  }
-  },
-  methods: {
-    todaydate: function() {
-      var d = new Date();
-      alert("Today: " + d.toDateString());
-	}
+    num1: 1,
+		num2: 2,
+    }
   }
 }
 </script>
@@ -18,13 +14,8 @@
     <div id="main">
       <!-- Ex. 1  -->
       <h2>Ex. 1</h2>
-      <p>Нажми</p>
-      <button @click="todaydate">Date</button>
+      <p>Сумма чисел {{ num1 }} и {{ num2 }} равна {{ this.num1 + this.num2 }}</p>
 
-      <!-- Ex. 2  -->
-      <h2>Ex. 2</h2>
-      <p>Наведи</p>
-      <button @mouseenter="todaydate">Date</button>
       <p id="endword">by NikitaRudenko</p>
     </div>
 </template>
@@ -40,35 +31,17 @@
     border-radius:50px;
     text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
-  #main p{
-    font-size: 25px;
+  #main p,   #main h2{
+    font-size: 40px;
+    margin-top: 10px;
   }
   #main h2{
-    font-size: 30px;
-    margin-top: 5px;
     text-decoration: underline;
   }
   #main #endword{
     font-size: 30px;
-    margin-top: 90px;
+    margin-top: 270px;
     color: rgb(83, 100, 80);
     text-shadow: 0px 1px 10px rgb(128, 151, 141);
-  }
-
-  #main button{
-    font-size: 40px;
-    color: rgb(26, 151, 9);
-    text-shadow: 0px 1px 10px rgb(90, 223, 112);
-    background-color: rgb(66, 255, 198);
-    height: 80px;
-    width: 160px;
-    border: 4px solid rgb(74, 197, 94);
-    border-radius: 40px;
-  }
-  #main button:hover{
-    color: rgb(57, 107, 41);
-    background-color: rgb(74, 197, 94);
-    border: 4px solid rgb(66, 255, 198);
-    text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
 </style>
