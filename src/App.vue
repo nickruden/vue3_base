70744572949c8ee492ce097e232253e4bf607d24
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 17:33:50 +0300

    Передача параметров в метод в Vue(Task_13)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <script>
- export default {
+ export default{
   name: "App",
   data() {
-    return {
-      date: 3,
-    }
+	return {
+	  }
   },
   methods: {
-    // Ex. 1
-    getDate: function () {
-      let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-      return week[this.date - 1];
-    },
-    
-    // Ex. 2
-    showDay: function () {
-      let weekday = this.getDate(this.date);
-      alert(weekday);
-    }
+    numalert: function(num) {
+      alert(num ** 2);
+	}
   }
 }
 </script>
 
 <template>
     <div id="main">
-      <!-- Ex. 1, Ex. 2  -->
-      <h2>Ex. 1, Ex. 2</h2>
-      <p>День недели {{ showDay() }}</p>
+      <!-- Ex. 1  -->
+      <h2>Ex. 1</h2>
+      <p>Для возведения числа 12 в квадрат нажмите на кнопку</p>
+      <button @click="numalert(12)">12</button>
 
       <p id="endword">by NikitaRudenko</p>
     </div>
@@ -43,17 +35,35 @@
     border-radius:50px;
     text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
-  #main p,   #main h2{
+  #main p{
     font-size: 40px;
-    margin-top: 10px;
   }
   #main h2{
+    font-size: 40px;
+    margin-top: 10px;
     text-decoration: underline;
   }
   #main #endword{
     font-size: 30px;
-    margin-top: 280px;
+    margin-top: 150px;
     color: rgb(83, 100, 80);
     text-shadow: 0px 1px 10px rgb(128, 151, 141);
+  }
+  #main button{
+    margin-top: 10px;
+    color: rgb(26, 151, 9);
+    font-size: 40px;
+    text-shadow: 0px 1px 10px rgb(108, 180, 84);
+    background-color: rgb(66, 255, 198);
+    height: 70px;
+    width: 140px;
+    border: 4px solid rgb(74, 197, 94);
+    border-radius: 30px;
+  }
+  #main button:hover{
+    color: rgb(57, 107, 41);
+    background-color: rgb(74, 197, 94);
+    border: 4px solid rgb(66, 255, 198);
+    text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
 </style>
