e07caa07f67267aeebd5f819baca541de81666c6
nickruden <1nikitos.ru2018@gmail.com>
2023-02-15 18:25:47 +0300

    Реактивное условие в Vue(Task_19)

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,41 @@
  export default{
   name: "App",
   data() {
-
   // Ex. 1
 	return {
     hidden: false,
-    textbutton: 'Изменить значение свойства на true'
+    text1: true,
+
+    //Ex. 3
+    text2: true,
+    button1: false,
+    button2: true
 	  }
   },
+
   methods: {
     clickbutton: function(){
       this.hidden = true
+    },
+
+    // Ex. 2
+    show1: function(){
+      this.text1 = true
+    },
+    hide1: function(){
+      this.text1 = false
+    },
+
+    // Ex. 3
+    show2: function(){
+      this.text2 = true,
+      this.button1 = false,
+      this.button2 = true
+    },
+    hide2: function(){
+      this.text2 = false,
+      this.button2 = false,
+      this.button1 = true
     }
   }
 }
@@ -21,13 +46,23 @@
     <div id="main">
       <!-- Ex. 1  -->
       <h2>Ex. 1</h2>
-      <p v-if="!hidden">Инвертируемое условие - показано, если значение свойства hidden будет равно false</p>
-      <p>Значение свойства 'hidden' равно {{ hidden }}</p>
+      <p v-if="hidden">Просто какой-то текст</p>
+      <button @click="clickbutton()">нажми</button>
 
-      <button @click="clickbutton()">{{ textbutton }}</button>
+      <!-- Ex. 2  -->
+      <h2>Ex. 2</h2>
+      <p v-if="text1">Просто какой-то текст</p>
+      <button @click="show1()">показать</button>
+      <button @click="hide1()">скрыть</button>
 
-      <p id="endword">by NikitaRudenko</p>
+      <!-- Ex. 3  -->
+      <h2>Ex. 3</h2>
+      <p v-if="text2">Просто какой-то текст</p>
+      <button v-if="button1" @click="show2()">показать</button>
+      <button v-if="button2" @click="hide2()">скрыть</button>
+
     </div>
+    <p id="endword">by NikitaRudenko</p>
 </template>
 
 <style>
@@ -35,23 +70,24 @@
     background-color: rgb(43, 221, 117);
     color: rgb(189, 255, 177);
     width: 1000px;
-    height: 500px;
+    height: 600px;
     text-align: center;
     border: 10px solid rgb(43, 221, 117);
-    border-radius:50px;
+    border-radius:60px;
     text-shadow: 0px 1px 10px rgb(160, 248, 207);
   }
   #main p{
-    font-size: 35px;
+    font-size: 22px;
   }
   #main h2{
-    font-size: 40px;
+    font-size: 30px;
     margin-top: 8px;
     text-decoration: underline;
   }
-  #main #endword{
+  #endword{
     font-size: 30px;
-    margin-top: 80px;
+    margin-top: 540px;
+    margin-left: -610px;
     color: rgb(83, 100, 80);
     text-shadow: 0px 1px 10px rgb(128, 151, 141);
   }
@@ -59,12 +95,13 @@
   #main button{
     margin-top: 15px;
     margin-right: 20px;
+    margin-left: 30px;
     color: rgb(26, 151, 9);
     font-size: 20px;
     text-shadow: 0px 1px 10px rgb(108, 180, 84);
     background-color: rgb(66, 255, 198);
-    height: 90px;
-    width: 250px;
+    height: 60px;
+    width: 150px;
     border: 4px solid rgb(74, 197, 94);
     border-radius: 40px;
   }
@@ -73,13 +110,5 @@
     background-color: rgb(74, 197, 94);
     border: 4px solid rgb(66, 255, 198);
     text-shadow: 0px 1px 10px rgb(160, 248, 207);
-  }
-
-  #main a{
-    font-size: 30px;
-    color: rgb(57, 107, 41);
-  }
-  #main a:hover{
-    color: rgb(233, 255, 255);
   }
 </style>
