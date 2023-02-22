7ca408306260961c6ddfd563eb89b64cfba84ea1
nickruden <1nikitos.ru2018@gmail.com>
2023-02-22 17:18:16 +0300

    Реактивность массивов в Vue(Task_35)

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,42 @@
   name: "App",
   data() {
     return {
-      // Ex. 1
-      products: [
-			  {
-				  id: 1,
-				  name: 'product1',
-			  },
-			  {
-				  id: 2,
-				  name: 'product2',
-			  },
-			  {
-				  id: 3,
-				  name: 'product3',
-			  },
-		  ],
+      // Ex. 1, 2, 3, 4, 5, 6
+      arr: [1, 2, 3, 4, 5],
     }
   },
-  methods: {
+  
 
+  methods: {
+    // Ex. 1
+    add: function() {
+      this.arr.push(this.arr.length+1);
+    },
+
+    // Ex. 2
+    delf: function() {
+      this.arr.shift();
+    },
+
+    // Ex. 3
+    dell: function() {
+      this.arr.pop();
+    },
+
+    // Ex. 4
+    delp: function() {
+      this.arr.splice(this.arr.length-2, 1);
+    },
+
+    // Ex. 5
+    sort: function() {
+      this.arr.sort();
+    },
+
+    // Ex. 6
+    rev: function() {
+      this.arr.reverse();
+    },
   }
 }
 </script>
@@ -30,13 +47,30 @@
   <div id="main">
     <!-- Ex. 1 -->
     <div>
-      <h2>Ex. 1</h2>
-      <ul v-for="product in products" :key="product.id">
-        <li>{{ product.name }}</li>
+      <h2>Ex. 1, 2, 3, 4, 5, 6 </h2>
+      <ul v-for="elem in  arr">
+        <li>{{ elem }}</li>
       </ul>
     </div>
+    <button @click="add">add</button>
+
+   <!-- Ex. 2 -->
+    <button @click="delf">del first</button>
+
+   <!-- Ex. 3 -->
+    <button @click="dell">del last</button>
+
+   <!-- Ex. 4 -->
+    <button @click="delp">del penult</button>
+
+   <!-- Ex. 5 -->
+    <button @click="sort">sort</button>
+
+   <!-- Ex. 6 -->
+    <button @click="rev">reverse order</button>
+
+    <p id="endword">by NikitaRudenko</p>
   </div>
-  <p id="endword">by NikitaRudenko</p>
 </template>
 
 <style>
@@ -46,14 +80,10 @@
     background-color: rgb(43, 221, 117);
     color: rgb(189, 255, 177);
     width: 1100px;
-    height: 500px;
     text-align: center;
     border: 10px solid rgb(43, 221, 117);
     border-radius:60px;
     text-shadow: 0px 1px 10px rgb(160, 248, 207);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
   }
   #main div{
     margin-top: 10px;
@@ -61,7 +91,7 @@
   #main p{
     font-size: 28px;
     margin-top: 20px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
   #main h2{
     font-size: 40px;
@@ -74,6 +104,7 @@
   #main ul{
     margin-top: 10px;
     text-align: left;
+    margin-left: 500px;
     font-size: 24px;
   }
   #main table{
@@ -89,10 +120,28 @@
     font-size: 24px;
     color: rgb(22, 88, 69);
   }
+
+  #main button{
+    margin-top: 20px;
+    margin-right: 20px;
+    color: rgb(26, 151, 9);
+    font-size: 20px;
+    text-shadow: 0px 1px 10px rgb(108, 180, 84);
+    background-color: rgb(66, 255, 198);
+    height: 50px;
+    width: 150px;
+    border: 4px solid rgb(74, 197, 94);
+    border-radius: 40px;
+  }
+  #main button:hover{
+    color: rgb(57, 107, 41);
+    background-color: rgb(74, 197, 94);
+    border: 4px solid rgb(66, 255, 198);
+    text-shadow: 0px 1px 10px rgb(160, 248, 207);
+  }
+
   #endword{
     font-size: 30px;
-    margin-top: 480px;
-    margin-left: -670px;
     color: rgb(83, 100, 80);
     text-shadow: 0px 1px 10px rgb(128, 151, 141);
   }
