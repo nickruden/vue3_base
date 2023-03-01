429a28e6b9683c412df1b7f4c1de3491bfc0ad33
nickruden <1nikitos.ru2018@gmail.com>
2023-03-01 19:40:58 +0300

    Создание дочерних компонентов в Vue(Task_58)

---
+++ src/components/Employee.vue
@@ -0,0 +1,24 @@
+<script>
+ export default{
+  name: "Employee",
+  data() {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  }
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style>
+
+</style>
